499d16cf2678ff4cbf6da5bc20af9170b4abdb8d
Ali Alrawi <ali.developer90@gmail.com>
2016-11-06 17:08:27 +0300

    update brand.list_devices method

--- lib/smartphone_finder/brand.rb
+++ lib/smartphone_finder/brand.rb
@@ -37,10 +37,7 @@ class SmartphoneFinder::Brand
 			end
 			counter+=3
 		end
-		# self.all.each do |brand|
-		# 	puts "#{counter}- #{brand.name}"
-		# 	counter+=1
-		# end
+
 	    puts "----------------------------------------------------------------"
 
 		
@@ -49,12 +46,33 @@ class SmartphoneFinder::Brand
 		self.devices << device
 	end
 	def list_devices
-		counter=1
-		self.devices.each do |device|
-			puts "#{counter}- #{device.name}"
-			counter +=1
-		end
-		puts "----------------------------------------------------------------"
+		counter=0
+		indention=" "
+		extra=" "	
+		width=30
+
+		while counter <self.devices.size
+			counter <9 ? extra=" " : extra=""
+			if self.devices.size - counter  >=3
+             col_1=self.devices[counter].name ; col_1_spc=indention*(width - col_1.length)
+             col_2=self.devices[counter+1].name ;col_2_spc=indention*(width - col_2.length)
+             col_3=self.devices[counter+2].name ;
+
+             puts "#{counter+1}- #{extra}#{col_1}#{col_1_spc}#{counter+2}- #{extra}#{col_2}#{col_2_spc}#{counter+3}- #{extra}#{col_3}"
+            elsif self.devices.size - counter  >=2
+             col_1=self.devices[counter].name ; col_1_spc=indention*(width - col_1.length)
+             col_2=self.devices[counter+1].name ;
+             puts "#{counter+1}- {extra}#{col_1}#{col_1_spc}#{counter+2}- #{extra}#{col_2}"
+            elsif self.devices.size - counter  >=1
+             col_1=self.devices[counter].name ;
+             puts "#{counter+1}- #{extra}#{col_1}"
+
 			end
+			counter+=3
+		end
+
+	    puts "----------------------------------------------------------------"
+
+	end
 	
 end
