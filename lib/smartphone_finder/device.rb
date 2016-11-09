5bcbac9b8458494e554c35ba267bb2e93777a846
Ali Alrawi <ali.developer90@gmail.com>
2016-11-09 09:54:21 +0300

    HelperMethods module has been created and linked to brand and device classes

--- lib/smartphone_finder/device.rb
+++ lib/smartphone_finder/device.rb
@@ -1,4 +1,7 @@
+require_relative '../concerns/helper_methods.rb'
+
 class SmartphoneFinder::Device
+	include HelperMethods
     @@all=[]
     attr_accessor :name ,:url ,:brand ,:specifications
 	def initialize(name,url,brand=nil)
@@ -10,9 +13,7 @@ class SmartphoneFinder::Device
         @@all.push(self)
        end
 	end
-    def obj_exist?
-    	@@all.detect{|obj| obj.name == self.name}
-    end
+
 	def self.all
 	   @@all 
     end
