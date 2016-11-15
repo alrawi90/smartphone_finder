11a4f8b0f5c9faf11fe0b1020f514d0901dd7fa2
Ali Alrawi <ali.developer90@gmail.com>
2016-11-15 03:20:43 +0300

    using HelperMethod.list_all for listing search results as well as listing brands and devices

--- lib/concerns/helper_methods.rb
+++ lib/concerns/helper_methods.rb
@@ -3,32 +3,62 @@ module HelperMethods
 def obj_exist?
    self.class.all.detect{|obj| obj.name == self.name}
  end
-def list_all #this method will print brands in 3 colomns
+def list_all(array,option) #this method will print brands in 3 colomns
 		counter=0
+		sep= "----------------------------------------------------------------"
 		indention=" "
 		extra=" "	
-		width=30
+        if option=="1"
+        	    object=array.max{|a, b| a.name.length <=> b.name.length}
+        	    width= object.name.length + 5
+				while counter <array.size
+					counter <9 ? extra=" " : extra=""
+					if array.size - counter  >=3
+		             col_1=array[counter].name ; col_1_spc=indention*(width - col_1.length)
+		             col_2=array[counter+1].name ;col_2_spc=indention*(width - col_2.length)
+		             col_3=array[counter+2].name ;
 
-		while counter <self.all.size
-			counter <9 ? extra=" " : extra=""
-			if self.all.size - counter  >=3
-             col_1=self.all[counter].name ; col_1_spc=indention*(width - col_1.length)
-             col_2=self.all[counter+1].name ;col_2_spc=indention*(width - col_2.length)
-             col_3=self.all[counter+2].name ;
+		             puts "#{counter+1}- #{extra}#{col_1}#{col_1_spc}#{counter+2}- #{extra}#{col_2}#{col_2_spc}#{counter+3}- #{extra}#{col_3}"
+		            elsif array.size - counter  >=2
+		             col_1=array[counter].name ; col_1_spc=indention*(width - col_1.length)
+		             col_2=array[counter+1].name ;
+		             puts "#{counter+1}- #{extra}#{col_1}#{col_1_spc}#{counter+2}- #{extra}#{col_2}"
+		            elsif array.size - counter  >=1
+		             col_1=array[counter].name ;
+		             puts "#{counter+1}- #{extra}#{col_1}"
 
-             puts "#{counter+1}- #{extra}#{col_1}#{col_1_spc}#{counter+2}- #{extra}#{col_2}#{col_2_spc}#{counter+3}- #{extra}#{col_3}"
-            elsif self.all.size - counter  >=2
-             col_1=self.all[counter].name ; col_1_spc=indention*(width - col_1.length)
-             col_2=self.all[counter+1].name ;
-             puts "#{counter+1}- #{extra}#{col_1}#{col_1_spc}#{counter+2}- #{extra}#{col_2}"
-            elsif self.all.size - counter  >=1
-             col_1=self.all[counter].name ;
-             puts "#{counter+1}- #{extra}#{col_1}"
+					end
+					counter+=3
+				end
+	
+			
+		else
+            object =array.max{|a, b| a.length <=> b.length}
+        	width=object.length + 5
+    		while counter <array.size
+	    	     counter <9 ? extra=" " : extra=""
+	             if array.size - counter  >=3
+	                 col_1=array[counter]; col_1_spc=indention*(width - col_1.length)
+	                 col_2=array[counter+1] ;col_2_spc=indention*(width - col_2.length)
+	                 col_3=array[counter+2];
 
-			end
-			counter+=3
-		end
+	                 puts "#{counter+1}- #{extra}#{col_1}#{col_1_spc}#{counter+2}- #{extra}#{col_2}#{col_2_spc}#{counter+3}- #{extra}#{col_3}"
+	              elsif array.size - counter  >=2
+	                 col_1=array[counter]; col_1_spc=indention*(width - col_1.length)
+	                 col_2=array[counter+1];
+	                 puts "#{counter+1}- {extra}#{col_1}#{col_1_spc}#{counter+2}- #{extra}#{col_2}"
+	              elsif array.size - counter  >=1
+	                 col_1=array[counter];
+	                 puts "#{counter+1}- #{extra}#{col_1}"
 
-	    puts "----------------------------------------------------------------"
-	end
+	    			    end
+	    			counter+=3
+	    		end
+	    		
+        end
+        2.times do
+          puts sep*2
+        end  
+    end
+		
 end
--- lib/smartphone_finder/brand.rb
+++ lib/smartphone_finder/brand.rb
@@ -30,35 +30,11 @@ class SmartphoneFinder::Brand
         devices.push(device)
       end
     end
-	
+	def self.list_brands
+		self.list_all(self.all,"1")	
+	end
 	def list_devices
-		counter=0
-		indention=" "
-		extra=" "	
-		width=30
-
-		while counter <self.devices.size
-			counter <9 ? extra=" " : extra=""
-			if self.devices.size - counter  >=3
-             col_1=self.devices[counter].name ; col_1_spc=indention*(width - col_1.length)
-             col_2=self.devices[counter+1].name ;col_2_spc=indention*(width - col_2.length)
-             col_3=self.devices[counter+2].name ;
-
-             puts "#{counter+1}- #{extra}#{col_1}#{col_1_spc}#{counter+2}- #{extra}#{col_2}#{col_2_spc}#{counter+3}- #{extra}#{col_3}"
-            elsif self.devices.size - counter  >=2
-             col_1=self.devices[counter].name ; col_1_spc=indention*(width - col_1.length)
-             col_2=self.devices[counter+1].name ;
-             puts "#{counter+1}- {extra}#{col_1}#{col_1_spc}#{counter+2}- #{extra}#{col_2}"
-            elsif self.devices.size - counter  >=1
-             col_1=self.devices[counter].name ;
-             puts "#{counter+1}- #{extra}#{col_1}"
-
-			end
-			counter+=3
-		end
-
-	    puts "----------------------------------------------------------------"
-
+	    self.list_all(self.devices,"1")
 	end
 	
 end
