8cf7d54c7ba9489b377024f3a7444a277bfc6925
Ali Alrawi <ali.developer90@gmail.com>
2016-12-02 10:14:15 +0300

    fixing indentation in HelperMethods module

--- lib/concerns/helper_methods.rb
+++ lib/concerns/helper_methods.rb
@@ -1,6 +1,6 @@
 module HelperMethods
 
-   def obj_exist?
+    def obj_exist?
         self.class.all.detect{|obj| obj.name == self.name}
     end
     def list_all(array) #this method will print lists in 3 colomns
@@ -12,12 +12,11 @@ module HelperMethods
             object =array.max{|a, b| a.length <=> b.length} #get the longest list entry
         	width=object.length + 5
     		while counter <array.size
-	    	     counter <9 ? extra=" " : extra=""
-	             if array.size - counter  >=3
+	    	      counter <9 ? extra=" " : extra=""
+	              if array.size - counter  >=3
 	                 col_1=array[counter]; col_1_spc=indention*(width - col_1.length)
 	                 col_2=array[counter+1] ;col_2_spc=indention*(width - col_2.length)
 	                 col_3=array[counter+2];
-
 	                 puts "#{counter+1}- #{extra}#{col_1}#{col_1_spc}#{counter+2}- #{extra}#{col_2}#{col_2_spc}#{counter+3}- #{extra}#{col_3}"
 	              elsif array.size - counter  >=2
 	                 col_1=array[counter]; col_1_spc=indention*(width - col_1.length)
@@ -27,14 +26,14 @@ module HelperMethods
 	                 col_1=array[counter];
 	                 puts "#{counter+1}- #{extra}#{col_1}"
 
-	    			    end
-	    			counter+=3
-	    		end
+	    		  end
+	    		  counter+=3
+	    	end
 	    		
 
             2.times do
-            puts sep*2
-        end  
+               puts sep*2
+            end  
     end
 		
 end
